37c8a8e3b9c64818fc7f8cb0aa73f0aa5d643eb7
alyhenr <alyssonhra99@gmail.com>
2023-08-16 12:33:23 -0300

    fix: redirect user to /timeline if logged in

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+
+import { UserContext } from "../contexts/userContext";
+
 import AuthPage from "../components/auth/authPage";
 
 export default function LoginPage() {
+    const navigate = useNavigate();
+    const { user } = useContext(UserContext);
+    useEffect(() => {
+        if (user?.token) {
+            navigate("/timeline");
+        }
+    }, []);
+
     return <AuthPage action={"signIn"} />
 }
