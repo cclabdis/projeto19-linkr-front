e56c7a6b5308bd04aa560be943904f79c9d1452d
augustof <augustobfanck@gmail.com>
2023-08-17 13:48:00 -0300

    feat: display postCards with mock, metadata missing

--- src/components/timeline/PostCard.jsx
+++ src/components/timeline/PostCard.jsx
@@ -24,8 +24,12 @@ export default function PostCard({post}){
                 <h1>{post.username}</h1>
                 <h2>{post.description}</h2>
                 <LinkContainer>
-
-                    <h2>{post.description}</h2>
+                    <div>
+                        <h3>{post.linkMetadata.title}</h3>
+                        <h4>{post.linkMetadata.header}</h4>
+                        <h5>{post.linkUrl}</h5>
+                    </div>
+                    <img src={post.linkMetadata.image} alt="metadata" />
                 </LinkContainer>
             </PostInfo>
         </Card>
@@ -41,7 +45,8 @@ const Card =  styled.div`
     display: flex;
     gap: 20px;
 
-    img {
+    >img {
+        flex: none;
         width: 50px;
         height: 50px;
         border-radius: 50%;
@@ -81,5 +86,51 @@ const LinkContainer = styled.div`
     height: 155px;
     border: 1px solid #4D4D4D;
     border-radius: 11px;
+    display: flex;
 
+    div {
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        justify-content: space-between;
+    }
+
+    img {
+        flex: none;
+        width: 155px;
+        height: 155px;
+        object-fit: cover;
+        border-radius: 0px 11px 11px 0px;
+    }
+
+    h3 {
+        font-family: Lato;
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 19px;
+        letter-spacing: 0em;
+        text-align: left;
+        color: #CECECE;
+    }
+
+    h4 {
+        font-family: Lato;
+        font-size: 11px;
+        font-weight: 400;
+        line-height: 13px;
+        letter-spacing: 0em;
+        text-align: left;
+        color: #9B9595;
+    }
+
+    h5 {
+        font-family: Lato;
+        font-size: 11px;
+        font-weight: 400;
+        line-height: 13px;
+        letter-spacing: 0em;
+        text-align: left;
+        color: #CECECE;
+    }
 `
--- src/pages/timelinePage.js
+++ src/pages/timelinePage.js
@@ -2,13 +2,21 @@ import React from "react";
 import TemplatePage from "../components/common/templatePage";
 import PublishBox from "../components/timeline/publishBox"; // Importe o PublishBox
 import PostCard from "../components/timeline/PostCard.jsx";
+import { styled } from "styled-components";
 
 const mockPost = {
     username: "Teste Nildo",
     photo: "https://picsum.photos/id/237/200/300",
     description: "Muito maneiro esse tutorial de Material UI com React, deem uma olhada!",
-    link: "https://medium.com/javascript-in-plain-english/i-created-the-exact-same-app-in-react-and-vue-here-are-the-differences-e9a1ae8077fd"
+    linkUrl: "https://medium.com/javascript-in-plain-english/i-created-the-exact-same-app-in-react-and-vue-here-are-the-differences-e9a1ae8077fd",
+    linkMetadata: { 
+        title: "Como aplicar o Material UI em um projeto React",
+        header: "Hey! I have moved this tutorial to my personal blog. Same content, new location. Sorry about making you click through to another page.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn5Ipwht9SgjUySSRE65fzvbr_6Ek2Gx-iyw&usqp=CAU"
+        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+    }
 }
+const posts = [mockPost, mockPost];
 
 export default function TimeLinePage() {
   const handlePublish = (newPost) => {
@@ -20,7 +28,18 @@ export default function TimeLinePage() {
     <TemplatePage title={`Time Line Page`} hasPublishBox={true}>
       <PublishBox onPublish={handlePublish} />
       {/* Outro conteúdo da timeline aqui */}
-      <PostCard post={mockPost}/>
+      <PostsContainer>
+                {posts.map(p =>
+                    <PostCard post={p}/>
+                )}
+            </PostsContainer>
     </TemplatePage>
   );
 }
+
+const PostsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`
