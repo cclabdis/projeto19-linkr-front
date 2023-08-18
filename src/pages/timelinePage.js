ae02d3c20fa1624ed1825ebb75940f9bfc25302a
augustof <augustobfanck@gmail.com>
2023-08-18 16:15:09 -0300

    feat: added loading to timeline

--- src/pages/timelinePage.js
+++ src/pages/timelinePage.js
@@ -6,32 +6,23 @@ import PostCard from "../components/timeline/PostCard.jsx";
 import { styled } from "styled-components";
 import SideBar from "../components/common/sideBar";
 import getPosts from "../services/apiPosts";
-
-// const mockPost = {
-//     username: "Teste Nildo",
-//     photo: "https://picsum.photos/id/237/200/300",
-//     description: "Muito maneiro esse tutorial de Material UI com React, deem uma olhada! #teste",
-//     linkUrl: "https://medium.com/javascript-in-plain-english/i-created-the-exact-same-app-in-react-and-vue-here-are-the-differences-e9a1ae8077fd",
-//     linkMetadata: { 
-//         title: "Como aplicar o Material UI em um projeto React",
-//         header: "Hey! I have moved this tutorial to my personal blog. Same content, new location. Sorry about making you click through to another page.",
-//         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn5Ipwht9SgjUySSRE65fzvbr_6Ek2Gx-iyw&usqp=CAU"
-//         //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
-//     }
-// }
-// const posts = [mockPost, mockPost];
+import ClipLoader from "react-spinners/ClipLoader";
 
 export default function TimeLinePage() {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(()=>{
-      getPosts()
-      .then((r)=>{
-          console.log(r.data);
-          setPosts(r.data);
+  useEffect(() => {
+    getPosts()
+      .then((r) => {
+        setPosts(r.data);
+        setIsLoading(false);
       })
-      .catch((err)=>{console.log(err.message)});
-  },[])
+      .catch((err) => {
+        console.log(err.message);
+        setIsLoading(false);
+      })
+  }, []);
 
   const handlePublish = (newPost) => {
     // Aqui você pode implementar a lógica para adicionar a nova publicação à timeline
@@ -44,11 +35,18 @@ export default function TimeLinePage() {
       {/* Outro conteúdo da timeline aqui */}
 
       <Container>
-        <PostsContainer>
-                  {posts.map(p =>
-                      <PostCard post={p}/>
-                  )}
-        </PostsContainer>      
+        {isLoading ?
+          <LoadingContainer>
+            <ClipLoader color="#fff" size={150} />
+            <h1 className="Oswald">Loading posts...</h1>
+          </LoadingContainer>
+          :
+          <PostsContainer>
+            {posts.map(p =>
+              <PostCard post={p} />
+            )}
+          </PostsContainer>
+        }
         <SideBar />
       </Container>
 
@@ -73,4 +71,18 @@ const PostsContainer = styled.div`
     flex-direction: column;
     align-items: center;
     gap: 20px;
+`
+const LoadingContainer = styled.div`
+    height: 345px;
+    width: 611px;
+    border-radius: 0 0 16px 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    
+    h1{
+      font-size: 35px;
+    }
 `
