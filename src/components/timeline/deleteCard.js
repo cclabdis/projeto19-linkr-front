b45488294364be2fde3a9029201db710c0a0d7fb
cclabdis <cclabdis@gmail.com>
2023-08-21 10:32:58 -0300

    reload delete

--- src/components/timeline/deleteCard.js
+++ src/components/timeline/deleteCard.js
@@ -2,6 +2,7 @@ import { deletePost } from "../../services/apiPosts";
 
 
 export const handleDelete = async (postId, token, refreshContext, setIsModalOpen) => {
+<<<<<<< Updated upstream
     console.log('postId:', postId)
     console.log('posttoken:', token)
     console.log(refreshContext)
@@ -14,4 +15,16 @@ export const handleDelete = async (postId, token, refreshContext, setIsModalOpen
         console.error('Erro ao excluir o post:', error);
         // Lide com o erro, se necessário
     }
+=======
+
+  try {
+    await deletePost(postId, token);
+    console.log("Post excluído com sucesso");
+    setIsModalOpen(false);
+    refreshContext.setRefresh((prevRefresh) => !prevRefresh);
+    window.location.reload();
+  } catch ({ message }) {
+    alert(message?.data || message);
+}
+>>>>>>> Stashed changes
 };
